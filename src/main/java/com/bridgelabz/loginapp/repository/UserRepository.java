114e1052a57ac5a747ee27d75081cbe2bab29b1f
rishibodake <rishibodake@gmail.com>
2020-05-23 10:19:36 +0530

    Added:The required fields

--- src/main/java/com/bridgelabz/loginapp/repository/UserRepository.java
+++ src/main/java/com/bridgelabz/loginapp/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.bridgelabz.loginapp.repository;
 
 import com.bridgelabz.loginapp.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findByEmail(String email);
 }
