5d6e2c1c03adf7892baf2bd31650d1d8f80a2b6a
rishibodake <rishibodake@gmail.com>
2020-05-23 10:10:28 +0530

    Added:register funtion in LoginRegisterService

--- src/main/java/com/bridgelabz/loginapp/service/LoginAndRegisterService.java
+++ src/main/java/com/bridgelabz/loginapp/service/LoginAndRegisterService.java
@@ -9,5 +9,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class LoginAndRegisterService {
 
+    @Autowired
+    private UserRepository userRepository;
 
+    public String register(User user) {
+
+        User findUser =  userRepository.findByEmail(user.getEmail());
+        if(findUser == null){
+            userRepository.save(user);
+            return "User Registered";
+        }
+        return "User Is Already Registered with this Email Id";
+    }
 }
